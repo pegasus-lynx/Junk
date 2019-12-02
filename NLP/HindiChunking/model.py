5da7187168df0a65206c29490a80e616391aa0fe
Shivansh Saini <shivanshs9@gmail.com>
2019-12-02 12:16:24 +0530

    SSF parser added + features changed

--- NLP/HindiChunking/model.py
+++ NLP/HindiChunking/model.py
@@ -3,12 +3,10 @@ plt.style.use('ggplot')
 
 from itertools import chain
 
-import nltk
 import sklearn
 import scipy.stats
 from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
 
 import sklearn_crfsuite
 from sklearn_crfsuite import scorers
@@ -19,29 +17,41 @@ import util as util
 
 # Setting the training and testing data
 
-train = util.convert("train.txt")
-test = util.convert("train.txt")
+# train = util.convertFromSSF("training_wx.txt")
+# test = util.convertFromSSF("testing_wx.txt")
 
-train_X = [features.sentToFeatures(s) for s in train]
-train_Y = [features.sentToLabels(s) for s in train]
+print('Loading data...')
 
-# s
+data = util.convertFromSSF('data_clean.txt')
+print(f'# of sentences = {len(data)}')
+data_x = [features.sentToFeatures(s) for s in data]
+data_y = [features.sentToLabels(s) for s in data]
 
-test_X = [features.sentToFeatures(s) for s in test]
-test_y = [features.sentToLabels(s) for s in test]
 
+print('Splitting data at 70%:30%...')
+train_X, test_X, train_Y, test_y = train_test_split(data_x, data_y, test_size=0.3, random_state=123)
+
+# train_X = [features.sentToFeatures(s) for s in train]
+# train_Y = [features.sentToLabels(s) for s in train]
+
+# test_X = [features.sentToFeatures(s) for s in test]
+# test_y = [features.sentToLabels(s) for s in test]
 
 # # Setting the model
 
 crf = sklearn_crfsuite.CRF(algorithm="lbfgs", c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
+
+print('Fitting the model...')
 crf.fit(train_X, train_Y)
 
 # labels = list(crf.classes_)
 
 # print(labels)
 
+print('Predicting labels...')
 y_pred = crf.predict(test_X)
 
+print('F1 score: ')
 print(metrics.flat_f1_score(test_y,y_pred,average='weighted'))
 
 # sorted_labels = sorted(
