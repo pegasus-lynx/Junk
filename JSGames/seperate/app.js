cdcd6fcff913f7acfc804883a8d7b9a05185879d
pegasus-lynx <dipeshkrj14@gmail.com>
2020-05-14 02:12:14 +0530

    Fixed the bugs in seperator game

--- JSGames/seperate/app.js
+++ JSGames/seperate/app.js
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Global Variables
   const wd = 800,
     ht = 400,
-    nballs = 60;
-  let sep, balls;
+    nballs = 30;
+  let sep;
+  let balls = [];
   let mouseX, mouseY;
+  let x_left, x_right;
   let colors = ["blue", "red"];
   let time;
   let loopInterval, timeInterval;
@@ -20,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     this.x = x;
     this.y = y;
     this.r = radius;
-    this.color = colors[Math.floor(math.random() * 2)];
+    this.color = colors[Math.floor(Math.random() * 2)];
     this.vx = -10 + Math.random() * 20;
     this.vy = -10 + Math.random() * 20;
 
@@ -32,86 +34,85 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     this.update_pos = () => {
-      let x_left = sep.x - sep.w / 2;
-      let x_right = sep.x + sep.w / 2;
-
 	  // If the ball collides with the seperator
-      if (sep.collide(this)) {
-		
-		this.vx *= -1;
-		
-		if(this.x < x_left){
-			this.x = x_left-this.r;
-			this.y += this.vy;
-		}
-		else{
-			this.x = x_right+this.r;
-		}
-	  } 
-	  else{
-		  this.bound();
-	  }
+      if (sep.collide(this)) {	
+        this.vx *= -1;
+        if(this.x < x_left){
+          this.x = x_left-this.r;
+          this.y += this.vy;
+        }
+        else{
+          this.x = x_right+this.r;
+          this.y += this.vy;
+        }
+      } 
+      else{
+        this.bound();
+      }
     };
 
     this.update = () => {
       this.update_pos();
       this.draw();
-	};
-	
-	this.bound = () => {
+    };
+    
+    this.bound = () => {
 
-		let flag_y=false, flag_x=false;
+      let flag_y=false, flag_x=false;
 
-		if(this.vy > 0 && this.y+this.r+this.vy > ht){
-			this.vy *= -1;
-			this.y = ht-this.r;
-			flag_y=true;
-		}
+      if(this.vy > 0 && this.y+this.r+this.vy > ht){
+        this.vy *= -1;
+        this.y = ht-this.r;
+        flag_y=true;
+      }
 
-		if(this.vy < 0 && this.y-(this.r+this.vy) < 0){
-			this.vy *= -1;
-			this.y = this.r;
-			flag_y = true;
-		}
+      if(this.vy < 0 && this.y-(this.r+this.vy) < 0){
+        this.vy *= -1;
+        this.y = this.r;
+        flag_y = true;
+      }
 
-		if(this.vx < 0 && this.x-(this.r+this.vx) < 0){
-			this.vx *= -1;
-			this.x = this.r;
-			flag_x = true;
-		}
+      if(this.vx < 0 && this.x-(this.r+this.vx) < 0){
+        this.vx *= -1;
+        this.x = this.r;
+        flag_x = true;
+      }
 
-		if(this.vx > 0 && this.x+(this.r+this.vx) > wd){
-			this.vx *= -1;
-			this.x = wd-this.r;
-			flag_x = true;
-		}
+      if(this.vx > 0 && this.x+(this.r+this.vx) > wd){
+        this.vx *= -1;
+        this.x = wd-this.r;
+        flag_x = true;
+      }
 
-		if(!flag_x){
-			this.x += this.vx;
-		}
+      if(!flag_x){
+        this.x += this.vx;
+      }
 
-		if(!flag_y){
-			this.y += this.vy;
-		}
-	}
+      if(!flag_y){
+        this.y += this.vy;
+      }
+    };
   }
 
   function Seperator(gap, x = wd / 2, y = ht / 2, width = 10) {
     this.x = x;
     this.y = y;
     this.w = width;
-    this.gap = gap;
+    this.g = gap;
+
+    // console.log(this.x, this.y, this.w, this.g);
+
+    x_left = this.x - (this.w/2);
+    x_right = this.x + (this.w/2);
 
     this.color = "rgb(100,100,100)";
 
     this.draw = () => {
-      x_left = this.x - this.width / 2;
-      h_upper = this.y - this.g / 2;
-      h_lower = ht - (this.y + this.g / 2);
-
+      h_upper = Math.max(0,this.y - (this.g / 2));
+      h_lower = Math.max(0,ht - (this.y + (this.g / 2)));
       stage.fillStyle = this.color;
-      stage.fillRect(x_left, 0, this.width, h_upper);
-      stage.fillRect(x_left, ht - h_lower, this.width, h_lower);
+      stage.fillRect(x_left, 0, this.w, h_upper);
+      stage.fillRect(x_left, ht - h_lower, this.w, h_lower);
     };
 
     this.update_pos = () => {
@@ -126,29 +127,26 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     this.collide = (ball) => {
-      let x_left = this.x - this.w / 2;
-      let x_right = this.x + this.w / 2;
-      let flag_x = false,
-        flag_y = false;
+      let flag_x = false, flag_y = false;
 
       if (ball.x < x_left && ball.vx > 0) {
         flag_x = ball.x + ball.vx > x_left - ball.r;
-      } else {
+      } 
+      else if(ball.x > x_right && ball.vx < 0) {
         flag_x = ball.x + ball.vx < x_right + ball.r;
       }
 
-      flag_y = ball.y < this.y - this.g / 2 + this.r / 2;
-      flag_y = ball.y > this.y + this.g / 2 - this.r / 2 || flag_y;
-
-      return flag_x && flag_y;
+      flag_y = ball.y < this.y - (this.g / 2) + (ball.r / 2);
+      flag_y = ball.y > this.y + (this.g / 2) - (ball.r / 2) || flag_y;
+      return (flag_y && flag_x);
     };
   }
 
-  function MouseEvent(event) {
-	  this.evt = event ? event : window.event;
-	  this.x = event.pageX ? event.pageX : event.clientX;
-	  this.y = event.pageY ? event.pageY : event.clientY;
-  }
+  // function MouseEvent(event) {
+	//   this.evt = event ? event : window.event;
+	//   this.x = event.pageX ? event.pageX : event.clientX;
+	//   this.y = event.pageY ? event.pageY : event.clientY;
+  // }
 
   // Create Balls
   function create_balls() {
@@ -184,7 +182,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (flag) {
       clearInterval(loopInterval);
       clearInterval(timeInterval);
-      console.alert("You won");
+      alert("You won");
     }
   }
 
@@ -200,20 +198,22 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function getMousePostion(event) {
-	  let e = MouseEvent(event);
-	  let r = canvas.getBoundingClientRect();
-	  mouseX = e.x - r.left;
-	  mouseY = e.y - r.top;
+	  // let e = MouseEvent(event);
+    let r = canvas.getBoundingClientRect();
+    let x = event.pageX || event.clientX;
+    let y = event.pageY || event.clientY;
+	  mouseX = x - r.left;
+    mouseY = y - r.top;
+    sep.update();
   }
 
   // Game Starting Function
   function init() {
     sep = new Seperator(60);
     create_balls();
-
-	time = 0;
+	  time = 0;
 	
-	window.onmousemove = getMousePostion;
+	  window.onmousemove = getMousePostion;
     loopInterval = setInterval(loop, 1000 / 30);
     timeInterval = setInterval(timer, 1000);
   }
